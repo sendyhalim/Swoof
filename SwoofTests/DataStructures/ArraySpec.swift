c9f35138962acfbb32c2e44b08a9dc44522c4f6a
Sendy Halim <sendyhalim93@gmail.com>
2015-12-18 00:40:10 +0700

    Add applicative apply (<*>) test

--- SwoofTests/DataStructures/ArraySpec.swift
+++ SwoofTests/DataStructures/ArraySpec.swift
@@ -14,9 +14,16 @@ class ArraySpec: QuickSpec {
   override func spec() {
     it("Should add two to all elements of array") {
       let xs = [1, 3, 5]
-      let addTwo = { $0 + 2 }
+      let addTwo = curry(+)(2)
 
       expect(addTwo <^> xs).to(equal([3, 5, 7]))
+    }
+
+    it("Should do non-deterministic calculation") {
+      let xs = [8, 20]
+      let fs = [curry(+)(2), curry(/)(4)]
+
+      expect(fs <*> xs).to(equal([10, 22, 2, 5]))
     }
   }
 }
