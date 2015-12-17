c7297cb716be76015f567853d87a3b53a4c45700
Sendy Halim <sendyhalim93@gmail.com>
2015-12-17 15:31:51 +0700

    Wrap assertion in

--- SwoofTests/DataStructures/ArraySpec.swift
+++ SwoofTests/DataStructures/ArraySpec.swift
@@ -12,9 +12,11 @@ import Quick
 
 class ArraySpec: QuickSpec {
   override func spec() {
-    let xs = [1, 3, 5]
-    let addTwo = { $0 + 2 }
+    it("Should add two to all elements of array") {
+      let xs = [1, 3, 5]
+      let addTwo = { $0 + 2 }
 
-    expect(addTwo <^> xs).to(equal([3, 5, 7]))
+      expect(addTwo <^> xs).to(equal([3, 5, 7]))
+    }
   }
 }
