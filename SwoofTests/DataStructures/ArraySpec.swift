f76b15bd0b2957d12db0764ac72c220895c95e4f
Sendy Halim <sendyhalim93@gmail.com>
2015-12-17 15:24:25 +0700

    Add basic test for array fmap (<^>) operator

---
+++ SwoofTests/DataStructures/ArraySpec.swift
@@ -0,0 +1,20 @@
+//
+//  ArraySpec.swift
+//  Swoof
+//
+//  Created by Sendy Halim on 12/17/15.
+//  Copyright © 2015 Sendy Halim. All rights reserved.
+//
+
+import Swoof
+import Nimble
+import Quick
+
+class ArraySpec: QuickSpec {
+  override func spec() {
+    let xs = [1, 3, 5]
+    let addTwo = { $0 + 2 }
+
+    expect(addTwo <^> xs).to(equal([3, 5, 7]))
+  }
+}
