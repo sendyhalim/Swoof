4d41b35b05542175734c0c62e99e3659eb6df21d
Sendy Halim <sendyhalim93@gmail.com>
2015-12-17 15:23:29 +0700

    Initial commit

---
+++ Swoof/Swoof.h
@@ -0,0 +1,19 @@
+//
+//  Swoof.h
+//  Swoof
+//
+//  Created by Sendy Halim on 12/17/15.
+//  Copyright © 2015 Sendy Halim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swoof.
+FOUNDATION_EXPORT double SwoofVersionNumber;
+
+//! Project version string for Swoof.
+FOUNDATION_EXPORT const unsigned char SwoofVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swoof/PublicHeader.h>
+
+
