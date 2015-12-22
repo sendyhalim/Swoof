23e6d656573e34c23015673e3123f52a60cef4b4
Sendy Halim <sendyhalim93@gmail.com>
2015-12-22 14:27:05 +0700

    Add groups to tests

--- SwoofTests/DataStructures/ArraySpec.swift
+++ SwoofTests/DataStructures/ArraySpec.swift
@@ -12,26 +12,40 @@ import Quick
 
 class ArraySpec: QuickSpec {
   override func spec() {
-    it("Should satisfy functor laws") {
-      let value = [8, 9, 11]
-      let idValue = id(value)
+    describe("Functor laws") {
+      it("should satisfy functor law `fmap id = id`") {
+        let value = [8, 9, 11]
+        let idValue = id(value)
 
-      expect(id <^> value).to(equal(idValue))
+        expect(id <^> value).to(equal(idValue))
+      }
+
+      it("should satisfy functor law `fmap (p . q) = (fmap p) . (fmap q)`") {
+        let value = [8, 9, 11]
+        let plusThree = curry(+)(3)
+        let minusTen = curry(-)(10)
+        let composed = plusThree • minusTen
+        let expected = minusTen <^> (plusThree <^> value)
+
+        expect(composed <^> value).to(equal(expected))
+      }
     }
 
-    it("Should add two to all elements of array") {
-      let xs = [1, 3, 5]
-      let addTwo = curry(+)(2)
+    describe("Functionality test") {
+      it("should add two to all elements of array") {
+        let xs = [1, 3, 5]
+        let addTwo = curry(+)(2)
 
-      expect(addTwo <^> xs).to(equal([3, 5, 7]))
-    }
+        expect(addTwo <^> xs).to(equal([3, 5, 7]))
+      }
 
-    it("Should do non-deterministic calculation") {
-      let xs = [8, 20]
-      let fs = [curry(+)(2), curry(/)(40)]
-      let expected = [10, 22, 5, 2]
+      it("should do non-deterministic calculation") {
+        let xs = [8, 20]
+        let fs = [curry(+)(2), curry(/)(40)]
+        let expected = [10, 22, 5, 2]
 
-      expect(fs <*> xs).to(equal(expected))
+        expect(fs <*> xs).to(equal(expected))
+      }
     }
   }
 }
