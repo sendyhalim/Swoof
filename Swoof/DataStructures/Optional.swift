a6579517525db5ea5631b183c81f0ae58758a866
Sendy Halim <sendyhalim93@gmail.com>
2015-12-25 12:22:49 +0700

    Add fmap, apply and bind operator for `Optional`

---
+++ Swoof/DataStructures/Optional.swift
@@ -0,0 +1,41 @@
+//
+//  Optional.swift
+//  Swoof
+//
+//  Created by Sendy Halim on 12/25/15.
+//  Copyright © 2015 Sendy Halim. All rights reserved.
+//
+
+import Foundation
+
+///  Fmap operator
+infix operator <^> { associativity left precedence 90 }
+
+public func <^> <A, B>(f: A -> B, value: A?) -> B? {
+  return value.map(f)
+}
+
+prefix operator <^> {}
+
+public prefix func <^> <A, B>(f: A -> B) -> A? -> B? {
+  return curry(<^>)(f)
+}
+
+///  Apply operator
+public func pure <A>(value: A) -> A? {
+  return .Some(value)
+}
+
+infix operator <*> { associativity left precedence 90 }
+
+public func <*> <A, B>(fs: (A -> B)?, value: A?) -> B? {
+  return fs.flatMap { $0 <^> value }
+}
+
+
+/// Bind operator
+infix operator >>= { associativity left precedence 90 }
+
+public func >>= <A, B>(value: A?, f: A -> B?) -> B? {
+  return value.flatMap(f)
+}
